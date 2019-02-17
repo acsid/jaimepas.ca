e0555850e703de34cab15c34ce77d6fe5aa0dcb6
acsid <drake@popalot.net>
2019-02-17 11:18:55 -0500

    encore plus de stats mais pour le moment c pomal ca

--- stats.php
+++ stats.php
@@ -1,13 +1,17 @@
 ﻿<?PHP
 use Medoo\Medoo;
 $fstats = $database->get("daystats","*",['date' => Medoo::raw('CURDATE()')]);
-printf("Stats: %s Choses pas aimer par %s personnes. Aujourd'hui %s Pages %s nouveau post %s nouveau votes et %s personne on cliquer sur al&eacute;atoire",
+printf("Stats: %s Choses pas aimer par %s personnes. Aujourd'hui %s Pages %s nouveau post %s nouveau votes et %s personne on cliquer sur al&eacute;atoire<br>
+		Totaux: %s pages vue %s page al&eacute;atoire en %s jours",
 		$database->count("sujet"),
 		$database->sum("sujet","vote"),
 		$fstats["pageview"],
 		$fstats["newpost"],
 		$fstats["newdontlike"],
-		$fstats["random"]
+		$fstats["random"],
+		$database->sum("daystats","pageview"),
+		$database->sum("daystats","random"),
+		$database->count("daystats")
 		);
 
 ?>
